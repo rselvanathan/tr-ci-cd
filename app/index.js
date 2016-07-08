e02e4170446a2b888d7e670a18fb70a9620949ad
Miguel Gonzalez <miguel.gonzalez@thomsonreuters.com>
2016-07-08 11:39:44 +0100

    Add code

--- app/index.js
+++ app/index.js
@@ -1,21 +1,46 @@
 const querystring = require('querystring');
 const request = require('request');
+const bodyParser = require("body-parser");
 const express = require('express');
 const app = express();
 
 const PORT = process.env.APP_PORT || 8080;
-const BACKEND = process.env.BACKEND_HOST;
+const BACKEND_HOST = process.env.BACKEND_HOST;
+const BACKEND_PORT = process.env.BACKEND_PORT || 8080;
+
+const baseUrl = `http://${BACKEND_HOST}:${BACKEND_PORT}/`;
+
+app.use(bodyParser.json());
 
 app.get('/api/:method', function (req, res) {
 	const qs = querystring.stringify(req.query);
-	const url = `http://${BACKEND}:8080/${req.params.method}?${qs}`;
+	const method = req.params.method;
+	const url = `${baseUrl}${method}?${qs}`;
+
 	request(url, function (err, _res, body) {
-		if (!err && _res.statusCode == 200) {
+		if (!err && _res.statusCode < 400) {
 			res.send(body);
 		} else {
 			res.send(500, "Backend is not available");
 		}
 	});
+});
+
+app.post('/api/addCity', function (req, res) {
+	request(
+		{
+			uri: `${baseUrl}cities`,
+			json: req.body,
+			method: 'POST'
+		},
+		function (err, _res, body) {
+			if (!err && _res.statusCode < 400) {
+				res.send({ok: true});
+			} else {
+				res.status(500).send("Backend is not available");
+			}
+		}
+	);
 });
 
 app.get('/', function (req, res) {
--- app/public/main.js
+++ app/public/main.js
@@ -1,16 +1,73 @@
-$(function () {
-	var $time_holder = $('.time__header');
+var $time_holder = $('.time__header');
+var $cities_holder = $('.cities');
 
-	fetch("/api/time")
-	.then((r) => {
-		if (r.status >= 400) {
-			$time_holder.addClass('alert alert-danger');
-		} else {
-			$time_holder.removeClass('alert alert-danger');
+var vm = new Vue({
+	el: '#app',
+	data: {
+		currentTime: new Date(),
+		offset: 0,
+		message: false,
+		error: false,
+		newCity: {},
+		cities: []
+	},
+	methods: {
+		add: function () {
+			console.log("Add!");
+			fetch("/api/addCity",
+				{
+					method: "POST",
+					headers: {'Content-Type': 'application/json'},
+					body: JSON.stringify(this.newCity)
+				}
+			).then(() => {this.updateCities();});
+			
+		},
+		updateCities: function () {
+			fetch("/api/cities")
+				.then((r) => {
+					if (r.status >= 400) {
+						$cities_holder.addClass('alert alert-danger');
+					} else {
+						$cities_holder.removeClass('alert alert-danger');
+					}
+					return r.json();
+				})
+				.then((data) => {
+					this.cities = data._embedded.cities.sort();
+				});
 		}
-		return r.text();
-	})
-	.then((text) => {
-		$('.time__header').text(text);
-	});
+	},
+	created: function () {
+		setInterval(() => {
+			var localTime = new Date();
+			this.currentTime = moment(localTime.getTime() + this.offset).format("dddd, MMMM Do YYYY, h:mm:ss a");
+		});
+
+		fetch("/api/time")
+			.then((r) => {
+				this.message = false;
+				if (r.status >= 400) {
+					this.message = {
+						type: 'danger',
+						text: r.text()
+					};
+				}
+				return r.json();
+			})
+			.then((time) => {
+				var remote = new Date(time.timestamp);
+				var local = new Date();
+				this.offset = remote.getTime() - local.getTime();
+				console.log("offset is", this.offset);
+			})
+			.catch((error) => {
+				this.message = {
+					type: 'danger',
+					text: error.message
+				};
+			});
+
+		this.updateCities();
+	}
 });
