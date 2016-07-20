103a91986bd44fd9d2cb304fdd3d0faf92a2468b
Romesh Selvanathan <Romesh.Selvanathan@thomsonreuters.com>
2016-07-20 13:35:51 +0100

    Made changes to the pipeline code and added a Beanstalk dockerrun template

--- be-pipeline.groovy
+++ be-pipeline.groovy
@@ -1,18 +1,12 @@
 node {
-	// Mark the code checkout 'stage'....
-	stage 'Checkout'
-
-	// Get some code from a GitHub repository
-	git url: 'https://github.com/miguelgonz/tr-ci-cd-be.git'
-
-	// Get the maven tool.
-	// ** NOTE: This 'M3' maven tool must be configured
-	// **       in the global configuration.           
 	def mvnHome = tool 'M3'
+	def dockerTag = (Branch.equals("master")) ? "latest" : Branch
+	
+	stage 'Checkout'
+	git url: 'https://github.com/miguelgonz/tr-ci-cd-be.git', branch : Branch
 
 	// Mark the code build 'stage'....
 	stage 'Build'
-	// Run the maven build
 	sh "${mvnHome}/bin/mvn -Dmaven.test.failure.ignore clean install"
 	step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
 
@@ -21,13 +15,12 @@ node {
 	sh "docker build -t unicorn-backend ."
 
 	stage 'Push Artifact'
-	sh "docker tag unicorn-backend rselvanathan/unicorn-backend:latest"
-	sh "docker push rselvanathan/unicorn-backend"
+	sh "docker tag unicorn-backend rselvanathan/unicorn-backend:${dockerTag}"
+	sh "docker push rselvanathan/unicorn-backend:${dockerTag}"
 
 	// Clean
 	sh "docker rmi unicorn-backend"
-	sh "docker rmi rselvanathan/unicorn-backend:latest"
-
-	stage 'Trigger Deploy'
-	build 'Deploy-to-AWS'
+	sh "docker rmi rselvanathan/unicorn-backend:${dockerTag}"
 }
+
+stage 'Trigger Image_Coordinator'
--- fe-pipeline.groovy
+++ fe-pipeline.groovy
@@ -1,13 +1,10 @@
 node {
-	// Mark the code checkout 'stage'....
+	def dockerTag = (Branch.equals("master")) ? "latest" : Branch
+
 	stage 'Checkout'
+	git url: 'https://github.com/miguelgonz/tr-ci-cd-fe.git' , branch : Branch
 
-	// Get some code from a GitHub repository
-	git url: 'https://github.com/miguelgonz/tr-ci-cd-fe.git'
-
-	// Mark the code build 'stage'....
 	stage 'Build'
-	// Run the npm build
 	sh "cd app; npm install; npm test;"
 
 	stage 'Create Artifact'
@@ -15,13 +12,12 @@ node {
 	sh "docker build -t unicorn-frontend ."
 
 	stage 'Push Artifact'
-	sh "docker tag unicorn-frontend rselvanathan/unicorn-frontend:latest"
+	sh "docker tag unicorn-frontend rselvanathan/unicorn-frontend:&{dockerTag}"
 	sh "docker push rselvanathan/unicorn-frontend"
 
 	// Clean
 	sh "docker rmi unicorn-frontend"
-	sh "docker rmi rselvanathan/unicorn-frontend:latest"
-
-	stage 'Trigger Deploy'
-	build 'Deploy-to-AWS'
+	sh "docker rmi rselvanathan/unicorn-frontend:&{dockerTag}"
 }
+
+stage 'Trigger Image_Coordinator'
